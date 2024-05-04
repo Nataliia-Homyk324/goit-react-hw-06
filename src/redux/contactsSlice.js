641b36ed5adb36338bd8defa1e58e248ba94a2d1
Nataliia-Homyk324 <homyk324@gmail.com>
2024-05-04 10:38:23 +0300

    slise contakt

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,28 +1,40 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { initialStateContacts } from "./constants";
 
 const contactsSlice = createSlice({
     name: 'contacts',
     initialState: initialStateContacts,
+    // initialState: {
+    //     contacts: {
+    //         items: []
+    //     }
+    // },
+
+    
+    
     reducers: {
         addContact: {
             reducer(state, action) {
+                //  state.items.push(action.payload)
                 state.push(action.payload)
             },
             prepare(values) {
                 return {
                     payload: {
                         ...values,
-                        id: nanoid(),
+                        id: crypto.randomUUID(),
                     }
                 }
             }
         },
+        
         deleteContact: (state, action) => {
+        // state.items = state.items.filter(contact => contact.id !== action.payload); 
             return state.filter(contact => contact.id !== action.payload);
         }
     }
-})
+    })
+
 
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
